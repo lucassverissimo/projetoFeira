3a65901257cc51f2bdff1f78522001aaed060555
Lucas Veríssimo Leite <lucass.verissimo@gmail.com>
2022-04-25 00:50:13 -0300

    iniciando a página de edição / criação da feira

--- projetofeiraapp/lib/app/views/pages/AdicionarFeiraPage.dart
+++ projetofeiraapp/lib/app/views/pages/AdicionarFeiraPage.dart
@@ -1,22 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:projetofeiraapp/app/models/FeiraModel.dart';
 
 import '../components/CustomAppBar.dart';
 
 class AdicionarFeiraPage extends StatefulWidget {
-  const AdicionarFeiraPage({Key? key}) : super(key: key);
+  
+  late FeiraModel feiraModel;
+
+  AdicionarFeiraPage({Key? key, FeiraModel? feiraModel}) : super(key: key);
 
   @override
-  State<AdicionarFeiraPage> createState() => _AdicionarFeiraPageState();
+  State<AdicionarFeiraPage> createState() => _AdicionarFeiraPageState(feiraModel);
 }
 
 class _AdicionarFeiraPageState extends State<AdicionarFeiraPage> {
+  _AdicionarFeiraPageState(FeiraModel feira);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
-      body: Column(children: [
-        Text('Adicionar Feira'),
-      ]),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(children: [
+          Expanded(
+            flex: 2,
+            child: TextField(     
+                                     
+              decoration: InputDecoration(
+                labelText: 'Título',
+                
+              ),
+            ),
+          ),
+          SizedBox(width: 4,),
+          Expanded(
+            flex: 1,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Data',
+              ),
+            )
+          
+          )
+        ]),
+      ),
     );
   }
 }
